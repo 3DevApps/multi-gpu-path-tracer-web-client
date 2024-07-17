5a10561cdab21a519eb172ace7aa7b45885bf86d
Patryk Klatka <patryk.klatka@gmail.com>
2024-07-17 23:14:20 +0200

    Add simple info about the project

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,19 +1,41 @@
-import React, { useCallback } from "react";
+import React, { useCallback, useState } from "react";
 import "./Header.css";
 import { GithubOutlined, QuestionCircleOutlined } from "@ant-design/icons";
+import { Modal } from "antd";
 
 export default function Header() {
   const handleGithubClick = useCallback(() => {
     window.open("https://github.com/3DevApps/multi-gpu-path-tracer");
   }, []);
 
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const closeModal = useCallback(() => {
+    setIsModalOpen(false);
+  }, []);
+  const openModal = useCallback(() => {
+    setIsModalOpen(true);
+  }, []);
+
   return (
     <div className="header-wrapper">
       <header className="header">Multi-GPU Path Tracer</header>
       <div className="header-buttons-wrapper">
-        <QuestionCircleOutlined className="header-button" />
+        <QuestionCircleOutlined onClick={openModal} className="header-button" />
         <GithubOutlined onClick={handleGithubClick} className="header-button" />
       </div>
+      <Modal
+        title="Multi-GPU Path Tracer"
+        open={isModalOpen}
+        onCancel={closeModal}
+        footer={null}
+      >
+        <p>
+          Engineering thesis project tackling raytracing in remote multi-gpu
+          environment.
+        </p>
+        <br />
+        <p>Authors: Patryk Klatka, Wojciech Łoboda, Bartłomiej Tempka</p>
+      </Modal>
     </div>
   );
 }
