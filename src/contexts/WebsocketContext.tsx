26a38c7ea0edfab4ed37b4b40b38052554a14d85
Patryk Klatka <patryk.klatka@gmail.com>
2024-09-25 20:37:31 +0200

    feat: change blob messages handler

--- src/contexts/WebsocketContext.tsx
+++ src/contexts/WebsocketContext.tsx
@@ -15,6 +15,20 @@ type WebsocketContextType = {
   sendMessage: (message: string[]) => void;
 };
 
+function getFormattedDateTime() {
+  const date = new Date();
+
+  const year = date.getFullYear();
+  const month = String(date.getMonth() + 1).padStart(2, "0");
+  const day = String(date.getDate()).padStart(2, "0");
+
+  const hours = String(date.getHours()).padStart(2, "0");
+  const minutes = String(date.getMinutes()).padStart(2, "0");
+  const seconds = String(date.getSeconds()).padStart(2, "0");
+
+  return `${year}-${month}-${day}_${hours}-${minutes}-${seconds}`;
+}
+
 export const WebsocketContext = createContext({} as WebsocketContextType);
 
 export const WebsocketContextProvider = ({ children }: any) => {
@@ -36,11 +50,19 @@ export const WebsocketContextProvider = ({ children }: any) => {
       console.log("WebSocket connection has been opened!");
     socket.current.onclose = () =>
       console.log("WebSocket connection has been closed!");
-    socket.current.onmessage = (event) => {
+    socket.current.onmessage = async (event) => {
       const rawData = event.data;
-      // If the data is a Blob, it must be an image to render
       if (rawData instanceof Blob) {
-        requestAnimationFrame(() => setRenderData(rawData));
+        // Read first 10 bytes to determine the message type
+        const text = await rawData.slice(0, 10).text();
+        if (text.startsWith("RENDER#")) {
+          requestAnimationFrame(() => setRenderData(rawData.slice(7)));
+        } else if (text.startsWith("SNAPSHOT#")) {
+          const a = document.createElement("a");
+          a.href = URL.createObjectURL(rawData.slice(9));
+          a.download = `view-${getFormattedDateTime()}.png`;
+          a.click();
+        }
         return;
       }
       setMessage(parseMessage(rawData));
